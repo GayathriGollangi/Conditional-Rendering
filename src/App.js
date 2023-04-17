4735b3b644a1605663793d446bedf1e833972984
gayathrigollangi <gayathrigollangii@gmail.com>
2023-04-17 07:27:59 +0000

    Using Ternary Operators

--- src/App.js
+++ src/App.js
@@ -3,21 +3,15 @@ import './App.css'
 import Welcome from './components/Welcome'
 
 class App extends Component {
-  state = {isLoggedIn: false}
+  state = {isLoggedIn: true}
 
   render() {
     const {isLoggedIn} = this.state
-    let authButton
-    if (isLoggedIn === true) {
-      authButton = <button>Logout</button>
-    } else {
-      authButton = <button>Login</button>
-    }
 
     return (
       <div className="container">
         <Welcome greeting="Hello" name="User" />
-        {authButton}
+        {isLoggedIn ? <button>Logout</button> : <button>Login</button>}
       </div>
     )
   }
