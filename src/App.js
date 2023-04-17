ebb19687fe402519bb15367beca641f27e6f7580
gayathrigollangi <gayathrigollangii@gmail.com>
2023-04-17 07:24:32 +0000

    Using Element Variables

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ import Welcome from './components/Welcome'
 class App extends Component {
   state = {isLoggedIn: false}
 
-  renderAuthButton = () => {
-    const {isLoggedIn} = this.state
-    if (isLoggedIn === true) {
-      return <button>Logout</button>
-    }
-    return <button>Login</button>
-  }
-
   render() {
+    const {isLoggedIn} = this.state
+    let authButton
+    if (isLoggedIn === true) {
+      authButton = <button>Logout</button>
+    } else {
+      authButton = <button>Login</button>
+    }
+
     return (
       <div className="container">
         <Welcome greeting="Hello" name="User" />
-        {this.renderAuthButton()}
+        {authButton}
       </div>
     )
   }
